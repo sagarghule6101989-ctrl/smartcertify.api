73388a9974731e1fdfb1a1323c14dc72ee7ead69
Sagar Ghule <sagar.ghule6101989@gmail.com>
2026-02-04 11:47:59 +0530

    Added StudentController API

---
+++ MyFirstWebAPI/MyFirstWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public IActionResult GetStudents()
+        {
+            var student = new[]
+            {
+                new {Id=1,Name="Sagar" },
+                new{ Id=2,Name="Rahul"}
+            };
+            return Ok(student);
+        }
+    }
+}
